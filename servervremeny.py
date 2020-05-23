566a52788521145d1b000385ae71a97a96c8a069
Noppik <55311174+Noppik@users.noreply.github.com>
2020-05-23 17:40:10 +0500

    Update servervremeny.py

--- servervremeny.py
+++ servervremeny.py
@@ -4,8 +4,8 @@ import time
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(('localhost', 123))
 lie_time = 0
-with open("lietime.txt", "r") as file:
-    lie_time = int(file.readline())
+with open("lietime.txt", "r") as f:
+    lie_time = int(f.readline())
 while True:
     data, address = server.recvfrom(1024)
     server.sendto(bytes(time.ctime(time.time() + lie_time), encoding="utf8"), address)
